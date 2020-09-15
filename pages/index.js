d8dd9af55db6dcf04dff635c70f64d0cef392b30
Tan Theng Lee <thengleetan@gmail.com>
2020-09-15 15:06:07 +0800

    Integrate Shopify app bridge for shopify user interfaces

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,47 @@
 import { EmptyState, Layout, Page, TextStyle } from '@shopify/polaris'
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react'
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg'
 
-const Index = () => {
-  return (
-    <Page>
-      <Layout>
-        <EmptyState
-          heading="Discount your products temporarily"
-          action={{
-            content: 'Select products',
-            onAction: () => console.log('clicked')
+const Index extends React.Component {
+  state = { open: false }
+
+  handleSelection = (resources) => {
+    this.setState({open: false})
+    console.log(resources)
+  }
+
+  render() {
+    return (
+      <Page>
+        <TitleBar
+          title="Sample App"
+          primaryAction={{
+            content: 'Select products'
           }}
-          image={img}
-        >
-          <p>Select products to change their price temporarily.</p>
-        </EmptyState>
-      </Layout>
-    </Page>
-  )
+        />
+          <ResourcePicker
+            resourceType="Product"
+            showVariants={false}
+            open={this.state.open}
+            onSelection={(resources) => this.handleSelection(resources)}
+            onCancel={() => this.setState({ open: false }) }
+          />
+        <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: 'Select products',
+              onAction: () => this.setState({ open: true })
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>
+      </Page>
+    )
+  }
 }
 
 export default Index
--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.prepare().then(() => {
       scopes: ['read_products'],
       afterAuth(ctx) {
         const { shop, accessToken } = ctx.session
+        ctx.cookies.set('shopOrigin', shop, {
+          httpOnly: false,
+          secure: true,
+          sameSite: 'none'
+        })
         ctx.redirect('/')
       }
     })
